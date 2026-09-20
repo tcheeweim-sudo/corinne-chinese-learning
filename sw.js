2e5d844fd9e5dd7a65333bbba8c190ae7cc359f9
tcheeweim-sudo <tcheeweim@gmail.com>
2026-09-20 18:15:39 +0800

    Add service worker

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C="corinne-v0-1",F=["./","./index.html","./manifest.webmanifest"];self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(F))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
